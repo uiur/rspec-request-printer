3f163881b2811b58bbc3f28d0b5935572d6374dc
Kazato Sugimoto <uiureo@gmail.com>
2021-01-20 19:41:26 +0900

    truncate too long output

--- lib/rspec/request/printer/hooks.rb
+++ lib/rspec/request/printer/hooks.rb
@@ -2,7 +2,17 @@ RSpec.configure do |config|
   config.after(:each) do |example|
     if example.metadata[:type] == :request && request && response && ENV['RSPEC_REQUEST_PRINTER']
       puts "\n#{request.request_method} #{request.original_url}"
-      puts JSON.pretty_generate(Rspec::Request::Printer.raw_request_params(request))
+
+      raw_params = Rspec::Request::Printer.raw_request_params(request)
+      raw_params = raw_params.map do |key, value|
+        if value.is_a?(String) && value.size > 200
+          [key, value[0..200] + '…']
+        else
+          [key, value]
+        end
+      end.to_h
+
+      puts JSON.pretty_generate(raw_params)
       puts
       if response.body.present?
         puts JSON.pretty_generate(response.parsed_body)
