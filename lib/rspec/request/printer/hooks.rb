46bc8bc3d77f8d721ec1e1c71620cf23fa1aeb89
Kazato Sugimoto <uiureo@gmail.com>
2021-04-19 18:31:33 +0900

    Show raw params for a request with body of json array

--- lib/rspec/request/printer/hooks.rb
+++ lib/rspec/request/printer/hooks.rb
@@ -5,6 +5,13 @@ RSpec.configure do |config|
       puts "\n#{request.request_method} #{request.original_url}"
 
       raw_params = Rspec::Request::Printer.raw_request_params(request)
+
+      # handle params for a request with body of json body
+      # for example: `[ { "name": "foo" }, { "name": "bar" } ]`
+      if raw_params.has_key?('_json')
+        raw_params = raw_params['_json']
+      end
+
       raw_params.each do |key, value|
         if value.is_a?(String) && value.size > 200
           raw_params[key] = value[0..200] + '…'
